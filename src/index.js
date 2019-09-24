7745b5234d29b3ef979df09eb2e35663498823f5
tehpsalmist <bstewardcodes@gmail.com>
2019-09-23 20:47:16 -0400

    setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { render } from 'react-dom'
 
 export const App = props => {
-  return <main>Hello World!</main>
+  const [count, setCount] = useState(0)
+
+  return <main>
+    <button
+      className='h-screen w-full bg-blue-500 text-white flex-center text-2xl'
+      onClick={e => setCount(count + 1)}
+    >
+      Click! {count}
+    </button>
+  </main>
 }
 
 render(<App />, document.getElementById('app'))
